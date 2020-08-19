f0541f783748d24855d03eb1035156eb3f692a7e
ZeptoZinc <68169274+ZeptoZinc@users.noreply.github.com>
2020-08-19 10:14:26 +0000

    Deploying to gh-pages from  @ b8dc848e3a4b0c19127ee65adaa4aa716aa1a25b 🚀

---
+++ precache-manifest.2d472b62685922d308d4180bd009f38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f15ee27f34ceb7ccf5914da227c386",
+    "url": "/democratizingdata/index.html"
+  },
+  {
+    "revision": "03cdec575fca26f63548",
+    "url": "/democratizingdata/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "679e2e9ca46b8304eb07",
+    "url": "/democratizingdata/static/js/2.bee648cf.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/democratizingdata/static/js/2.bee648cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cdec575fca26f63548",
+    "url": "/democratizingdata/static/js/main.ee0582f2.chunk.js"
+  },
+  {
+    "revision": "4800fab7df11393ce021",
+    "url": "/democratizingdata/static/js/runtime-main.80c43f10.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2f375f072e29cab303273a7f8548571.js"
+  "/democratizingdata/precache-manifest.2d472b62685922d308d4180bd009f38c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/democratizingdata/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
